109dc7d0487c3de8debd4abbc39c9a67d99b3f19
angelinaaat <atimchuk2410@gmail.com>
2025-03-06 05:20:14 +0200

    update js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-catalog = document.querySelectorAll('.catalog')
-navigationTwo = document.querySelector('.navigation2')
+let catalog = document.querySelectorAll('.category')
+let catalog2= document.querySelectorAll('.catalog2')
+let all = document.querySelector('.all');
+all.addEventListener('mousemove',function(){
+        all.style.background = '#f2760a';
+        all.style.color = '#ffffff';
+        // all.style.border= 10;
+        catalog.style.display = 'block'
+        catalog2.style.display = 'block'
 
+})
+
+let confis = document.querySelector('.confiseurs');
+confis.addEventListener('mousemove',function(){
+        confis.style.background = '#f2760a';
+        confis.style.color = '#ffffff';
+})
+
+let caramel = document.querySelector('.caramel');
+caramel.addEventListener('mousemove',function(){
+        caramel.style.background = '#f2760a';
+        caramel.style.color = '#ffffff';
+})
+
+let popular = document.querySelector('.popular');
+popular.addEventListener('mousemove',function(){
+        popular.style.background = '#f2760a';
+        popular.style.color = '#ffffff';
+})
+
+let fudge = document.querySelector('.fudge');
+fudge.addEventListener('mousemove',function(){
+        fudge.style.background = '#f2760a';
+        fudge.style.color = '#ffffff';
+})
+
+all.addEventListener('click', function(){
+    catalog.style.display = 'block';
+    catalog2.style.display = 'block';
+    // navItems.forEach(nav => {
+    //     nav.addEventListener("click", function () {
+    //         const category = this.getAttribute("data-category");
+            
+    //         productItems.forEach(product => {
+    //             if (category === "all" || product.classList.contains(category)) {
+    //                 product.style.display = "block";
+    //             } else {
+    //                 product.style.display = "none";
+    //             }
+    //         });
+    //     });
+    // });
+    });
+        
